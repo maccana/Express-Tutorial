5a8dd45242cdaf0cea8dd1c43dee2da55ac61e0b
maccana <mikeymccann@gmail.com>
2017-06-10 17:04:23 -0400

    Added tests directory

---
+++ tests/index.js
@@ -0,0 +1,10 @@
+var stuff = { 
+        accepted: [ 'item1', 'item2' ],
+        rejected: [],
+        response: 'Foo',
+        envelope: { from: 'The sender', to: ['new item1', 'new item2'] },
+        messageId: 'xxxxxxxxxxxxx' 
+}
+
+
+console.log('Log Stuff.... ', stuff);
