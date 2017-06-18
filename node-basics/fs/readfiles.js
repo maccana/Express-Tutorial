83ce071a68b629751982dd95cd79bdb647fb0af9
maccana <mikeymccann@gmail.com>
2017-06-18 16:42:12 -0400

    Added fs/readfiles.js example

--- node-basics/fs/readfiles.js
+++ node-basics/fs/readfiles.js
@@ -2,4 +2,4 @@ var fs = require('fs');
 
 var contents = fs.readFileSync('./lib/notes.md', 'UTF-8');
 
-console.log('READ FILE WITH FS :', contents);
+console.log('READ FILE CONTENTS WITH FS :', contents);
