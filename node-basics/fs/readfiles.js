42ed20d4e4e14b80bdeea57f3ae9e787ac418449
maccana <mikeymccann@gmail.com>
2017-06-18 16:51:26 -0400

    Updated readfiles.js with fs.readFile async example

--- node-basics/fs/readfiles.js
+++ node-basics/fs/readfiles.js
@@ -3,3 +3,18 @@ var fs = require('fs');
 var contents = fs.readFileSync('./lib/notes.md', 'UTF-8');
 
 console.log('READ FILE CONTENTS WITH FS :', contents);
+
+// Using fs.readFile async
+
+var contents = fs.readFile('./lib/asyncread.md', 'UTF-8', function(err, contents) {
+    if(err) {
+        console.log('Throwing the error will cause the error to get throw and kill the process');
+        console.log('=========================================================================');
+        console.log('There was an error reading the file...', err);
+        
+        
+    }
+    console.log('READ FILE CONTENTS ASYNCRONOUSLY WITH FS :', contents);
+    
+});
+
