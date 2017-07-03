be21f166f3f8aae547b86722e8a1f3fe1fa7dde1
maccana <mikeymccann@gmail.com>
2017-07-02 23:55:21 -0400

    Added promiseAll_1.js

--- node-basics/promises/promiseAll_1.js
+++ node-basics/promises/promiseAll_1.js
@@ -2,9 +2,9 @@ const getStuff = require('./getstuff');
 const path = require('path');
 
 Promise.all([
-    getStuff(path.join(__dirname, 'albums.txt')),
-    getStuff(path.join(__dirname, 'artists.txt')),
-    getStuff(path.join(__dirname, 'lyrics.txt'))
+    getStuff(path.join(__dirname, 'data', 'albums.txt')),
+    getStuff(path.join(__dirname, 'data', 'artists.txt')),
+    getStuff(path.join(__dirname, 'data', 'lyrics.txt'))
 
 ]).then((values => {
     let albums = values[0]
@@ -12,5 +12,4 @@ Promise.all([
     let lyrics = values[2]  
     console.log(albums + '\n' + artists + '\n' + lyrics)
 }))
-
 
