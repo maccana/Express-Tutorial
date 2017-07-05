711b1f91e50c2c608220c399b79d685a41e41f1e
maccana <mikeymccann@gmail.com>
2017-07-04 23:37:00 -0400

    Modified rename.js to create files if they don't exist

--- node-basics/fs/rename.js
+++ node-basics/fs/rename.js
@@ -1,14 +1,42 @@
 var fs = require('fs');
 
-// synchronous
-fs.renameSync('./lib/project-config.js', './lib/config.json');
+// Check the file exists
+fs.stat('./lib/project-config.js', function (err, stat) {
+    if (err == null) {
+        console.log('File exists');
+        // Call synchronous file rename
+        renameFileSync();
+    } else if (err.code == 'ENOENT') {
+        // Create the file
+        fs.writeFile('./lib/project-config.js', 'Some log\n');
+        renameFileSync();
+    } else {
+        console.log('Some other error: ', err.code);
+    }
+});
+
+function renameFileSync() {
+    fs.renameSync('./lib/project-config.js', './lib/config.json');
+}
+
+function renameFileAsync() {
+    // asyncrhonous
+    fs.rename('./lib/notes.md', './notes.md', function (err) {
+        if (err) {
+            console.log(err)
+        }
+        console.log('File was renamed and moved to different location...');
+    })
+}
 
 console.log('Config file renamed..');
 
-// asyncrhonous
-fs.rename('./lib/notes.md', './notes.md', function(err) {
-    if(err) {
-        console.log(err)
-    }
-    console.log('File was renamed and moved to different location...');
-})
+if (!fs.existsSync('./lib/notes.md')) {
+    // Create the file
+    fs.writeFile('./lib/notes.md', 'Some notes here.....\n');
+    renameFileAsync();
+
+} else {
+    renameFileAsync();
+}
+
