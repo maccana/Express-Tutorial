0cdb24775d3c4be380b74cb14b82872a68825f0a
System Administrator <root@MikeMac.local>
2017-08-30 13:27:23 -0400

    Added satoshi btc project folder

---
+++ project_apps/satoshiworth/README.md
@@ -0,0 +1,3 @@
+Bitcoin to the moon price converter.
+ 
+
---
+++ project_apps/satoshiworth/app.js
@@ -0,0 +1,21 @@
+var express = require('express');
+var request = require('request');
+
+var app = express();
+
+request({
+    url: "https://blockchain.info/stats?format=json",
+    json: true
+  }, function(error, response, body){
+      btcoinPrice = body.market_price_usd;
+      btcBlocks = body.n_blocks_total;
+  }
+);
+
+app.get("/", function(req, res){
+  res.send("Bitcoin to the moon: " + btcoinPrice);
+});
+
+app.listen(8001, function(){
+  console.log("Bitcoin to the moon listening on port 8001....");
+});
